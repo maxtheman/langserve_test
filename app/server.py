4de70f034868ebafc95b2896cddd4bb07fe3f2df
Max Caldwell <max@scriptt.ai>
2023-11-21 13:57:26 -0800

    fixed pirate app ARR

--- app/server.py
+++ app/server.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 
+from pirate_speak.chain import chain as pirate_speak_chain
+
 app = FastAPI()
 
 
@@ -11,7 +13,7 @@ async def redirect_root_to_docs():
 
 
 # Edit this to add the chain you want to add
-add_routes(app, NotImplemented)
+add_routes(app, pirate_speak_chain, path="/pirate-speak")
 
 if __name__ == "__main__":
     import uvicorn
